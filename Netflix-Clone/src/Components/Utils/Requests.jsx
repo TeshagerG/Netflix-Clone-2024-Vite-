f6fd4f8f6959901e5708ad5e46f7dbe78cf5823c
TeshagerG <teshager.gedamu@gmail.com>
2024-12-17 11:12:02 +0800

    env

--- Netflix-Clone/src/Components/Utils/Requests.jsx
+++ Netflix-Clone/src/Components/Utils/Requests.jsx
@@ -1,14 +1,14 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 const Requests = {
-    fetchTrending: `https:/api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `https:/api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchTopRatedMovies: `https:/api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchActionMovies: `https:/api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
-    fetchComedyMovies: `https:/api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
-    fetchHorrorMovies: `https:/api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
-    fetchRomanceMovies: `https:/api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-    fetchDocumentaries: `https:/api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
-    fetchTvShow: `https:/api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
+    fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=en-US&page=1`
   };
   export default Requests;
--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Netflix-Clone-2024-Vite-/",
-  server:{
-    port:5000,
-  }
+  // server:{
+  //   port:5000,
+  // }
 })
